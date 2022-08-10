3bec5b1ab88a389a2bb5e97f40777da78a4987c3
Deepanshi <44083363+deepanshi-s@users.noreply.github.com>
2022-08-09 21:33:09 -0700

    updated loss function

--- lossFunction.py
+++ lossFunction.py
@@ -20,9 +20,8 @@ def lossFunction(pred, target):
     boxLoss = modulatedLoss(pred[:, 1:], target[:, 1:])
     boxLoss[indexNoStar] = 0
     
-    classLoss = F.binary_cross_entropy_with_logits(pred[:, 0], target[:, 0], reduction='none')
-    
-    totalLoss = boxLoss + classLoss
+    classLoss = torch.nn.BCELoss(reduction='none')(pred[:, 0], target[:, 0])
+    totalLoss = 0.5*boxLoss + 0.5*classLoss
     
     return totalLoss, boxLoss, classLoss
     
@@ -43,9 +42,9 @@ def modulatedLoss(pred, target):
     x_true, y_true, yaw_true, w_true, h_true = target[:, 0], target[:, 1], target[:, 2], target[:, 3], target[:, 4]
     x_pred, y_pred, yaw_pred, w_pred, h_pred = pred[:, 0], pred[:, 1], pred[:, 2], pred[:, 3], pred[:, 4]
     
-    loss = torch.min(
-        torch.abs(x_true - x_pred) + torch.abs(y_true - y_pred) + torch.abs(w_true - w_pred) + torch.abs(h_true - h_pred) + torch.abs(yaw_true - yaw_pred),
-        torch.abs(x_true - x_pred) + torch.abs(y_true - y_pred) + torch.abs(w_true - h_pred) + torch.abs(h_true - w_pred) + torch.abs(180 - torch.abs(yaw_true - yaw_pred))
+    loss = torch.abs(x_true - x_pred) + torch.abs(y_true - y_pred) + torch.abs(w_true - w_pred) + torch.abs(h_true - h_pred) + torch.min(
+        torch.abs(yaw_true - yaw_pred),
+        torch.abs(0.5 - torch.abs(yaw_true - yaw_pred))
     )
-        
+
     return loss
